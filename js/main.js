bd8ee5ca3d5dabcd440a6376bab3594ef1c7d0ef
Gustavo <gustavohks2@gmail.com>
2017-07-22 12:05:00 -0300

    Set update tags of appliants function

--- js/main.js
+++ js/main.js
@@ -1,3 +1,23 @@
 (function ManageSorter() {
 
+  const applicants = [];
+  let template = "";
+
+  this.init = function() {
+    updateApplicants();
+  }
+
+  this.updateApplicants = function() {
+    var tagsContainer = document.querySelector("#tags-container");
+    template = "";
+
+    applicants.forEach(function(applicant) {
+      template += '<span class="applicant" style="background-color: #FFC153;">' + applicant + '</span>';
+    });
+
+    tagsContainer.innerHTML = "";
+    tagsContainer.insertAdjacentHTML("afterbegin", template);
+  }
+
+  init();
 })();
